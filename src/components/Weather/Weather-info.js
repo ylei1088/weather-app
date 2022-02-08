a89a42c5190b0a9f6e43689c7b2a5ac97d819ee4
ylei1088 <ylei1088@gmail.com>
2022-02-07 22:56:38 -0600

    change fahrenheit to celsius

--- src/components/Weather/Weather-info.js
+++ src/components/Weather/Weather-info.js
@@ -10,22 +10,26 @@ function WeatherInfo(props) {
     `${data.cod !== 404 ? data.weather[0].icon : null}` +
     ".png";
 
+  //change fahrenheit to celsius
+  const mainfahrenheit = Math.floor(data.main.temp - 273) + 32;
+  const fellLike_fahrenheit = Math.floor(main.feels_like - 273) + 32;
+  const minfahrenheit = Math.round(main.temp_min - 273) + 32;
+  const maxfahrenheit = Math.round(main.temp_max - 273) + 32;
+
   return (
     <div className="container">
       <div className="weather-box">
         {" "}
         <h3>Current Weather: {name}</h3>
         <div className="WeatherContainer">
-          <h1>{Math.floor(data.main.temp - 273) + 32}°F </h1>
+          <h1>{mainfahrenheit + "°F"}</h1>
           {`  |  ${data?.weather[0].description}`}
 
           <img className="weather-icon" src={iconurl} alt="" />
         </div>
-        <p>Feels Like: {Math.floor(main.feels_like - 273) + 32}°F</p>
+        <p>{"Feels Like: " + fellLike_fahrenheit + "°F"}</p>
         <p>
-          Min: {Math.round(main.temp_min - 273) + 32}˚F / Max:{" "}
-          {Math.round(main.temp_max - 273) + 32}
-          ˚F
+          {minfahrenheit + "°F"} / {maxfahrenheit + "°F"}
         </p>
         <p>Humidity: {main.humidity}%</p>
       </div>
